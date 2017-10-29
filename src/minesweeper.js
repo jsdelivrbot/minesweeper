e4557466841c5336d2507aa2096d81565c026669
Sam Gallant <sgallant@gmail.com>
2017-10-29 20:50:59 +0800

    complete replacement

---
+++ src/minesweeper.js
@@ -0,0 +1,13 @@
+let blankLine = '  |   |  ';
+let guessLine = '1 |   |  ';
+let bombLine = '  | B |  '
+
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+console.log('This is what an empty board would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
